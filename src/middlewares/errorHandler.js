5d7cc3c6493ad9d27f1fdb7433f38ccec4444227
TanyaTymoshchenko <ttymoshchenko@yahoo.com>
2024-07-26 22:47:01 +0300

    update

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,26 @@
+import { HttpError } from 'http-errors';
+
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
+    });
+    return;
+  }
+
+  res.status(500).json({
+    status: 500,
+    message: 'Something went wrong',
+    data: err.message,
+  });
+  next();
+};
+
+export const notFoundHandler = (err, req, res, next) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+  next();
+};
